de0ebd18ebbb67a546adf925967fe8152da9dbdc
Albert <xalbert.einsteinx@gmail.com>
2017-04-05 10:43:11 +0300

    Add OrderAsset class on frontend.

---
+++ frontend/assets/OrderAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace xalberteinsteinx\shop\frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset for OrderController actions
+ */
+class OrderAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/xalberteinsteinx/shop/frontend/web';
+
+    public $css = [
+        'css/order.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+    ];
+}
