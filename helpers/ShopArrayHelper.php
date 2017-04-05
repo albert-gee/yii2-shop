64a76e65ca8ce2dd1130314ed55104b25159c9ac
Albert <xalbert.einsteinx@gmail.com>
2017-04-05 09:25:11 +0300

    Add ShopArrayHelper.

---
+++ helpers/ShopArrayHelper.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+
+namespace xalberteinsteinx\shop\helpers;
+
+/**
+ * Removes duplicated elements from multidimensional array.
+ *
+ * Class ShopArrayHelper
+ * @package xalberteinsteinx\shop\helpers
+ * @returns array
+ */
+class ShopArrayHelper
+{
+    public static function removeDuplicatedArrayElements($array) {
+        $result = array_reduce($array, function($a, $b) {
+            static $stored = [];
+
+            $hash = md5(serialize($b));
+
+            if (!in_array($hash, $stored)) {
+                $stored[] = $hash;
+                $a[] = $b;
+            }
+
+            return $a;
+        }, []);
+
+        return $result;
+    }
+}
