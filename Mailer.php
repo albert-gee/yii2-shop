029076a948b073a43c816a34866b7e8967a6f34d
Albert <xalbert.einsteinx@gmail.com>
2017-04-05 09:37:46 +0300

    Add Mailer class and UrlRule class.

---
+++ Mailer.php
@@ -0,0 +1,205 @@
+<?php
+namespace xalberteinsteinx\shop;
+
+use Exception;
+use yii\base\Component;
+use yii\helpers\{
+    Html, Url
+};
+use bl\multilang\entities\Language;
+use bl\emailTemplates\data\Template;
+use xalberteinsteinx\shop\common\entities\Product;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class Mailer extends Component
+{
+
+    /**
+     * Uses emailTemplates component which parses variables
+     * and inserts values to email subject and body
+     * @param string $mailKey
+     * @param array $mailVars
+     * @return mixed
+     */
+    private function createMailTemplate(string $mailKey, array $mailVars = [])
+    {
+
+        /**
+         * @var $mailTemplate Template
+         */
+        $mailTemplate = \Yii::$app->get('emailTemplates')
+            ->getTemplate($mailKey, Language::getCurrent()->id);
+        if (!empty($mailVars) && !empty($mailTemplate)) {
+            $mailTemplate->parseSubject($mailVars);
+            $mailTemplate->parseBody($mailVars);
+        }
+
+        return $mailTemplate;
+    }
+
+    /**
+     * @param $sendFrom
+     * @param $sendTo
+     * @param string $bodySubject
+     * @param array $bodyParams
+     * @throws Exception
+     */
+    private function sendMessage($sendFrom, $sendTo, string $bodySubject, array $bodyParams = [])
+    {
+        if (!empty($sendTo)) {
+            try {
+                \Yii::$app->get('shopMailer')->compose('mail-body', $bodyParams)
+                    ->setFrom($sendFrom)
+                    ->setTo($sendTo)
+                    ->setSubject($bodySubject)
+                    ->send();
+
+            } catch (Exception $ex) {
+                throw new Exception($ex);
+            }
+        }
+    }
+
+    /**
+     * Sends mail about partner request to manager
+     * @param array $mailVars
+     * @param $sendFrom
+     * @param $sendTo
+     */
+    public function sendPartnerRequestToManager(array $mailVars, $sendFrom, $sendTo)
+    {
+
+        $mailTemplate = $this->createMailTemplate('partner-request-manager', $mailVars);
+
+        if (!empty($mailTemplate)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $this->sendMessage($sendFrom, $sendTo, $subject, $bodyParams);
+        }
+
+
+    }
+
+    /**
+     * Sends mail about partner request to partner
+     * @param array $mailVars
+     * @param array $sendFrom
+     * @param array $sendTo
+     */
+    public function sendPartnerRequestToPartner(array $mailVars, $sendFrom, $sendTo)
+    {
+
+        $mailTemplate = $this->createMailTemplate('partner-request-partner', $mailVars);
+
+        if (!empty($mailTemplate)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $this->sendMessage($sendFrom, $sendTo, $subject, $bodyParams);
+        }
+
+
+    }
+
+    /**
+     * Sends mail to partner if his request was approved
+     * @param string $partnerEmail
+     */
+    public function sendPartnerAcceptance(string $partnerEmail)
+    {
+        $mailTemplate = $this->createMailTemplate('partner-request-accept');
+
+        if (!empty($mailTemplate)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $sendFrom = [\Yii::$app->get('shopMailer')->transport->getUsername() => \Yii::$app->name ?? parse_url(Url::base(true), PHP_URL_HOST)];
+
+            $this->sendMessage($sendFrom, $partnerEmail, $subject, $bodyParams);
+        }
+
+    }
+
+    /**
+     * If user which has not 'createProductWithoutModeration' permission
+     * email to manager and this user will be sent
+     * @param Product $product
+     */
+    public function sendNewProductToManagerAndOwner(Product $product)
+    {
+        $productOwner = $product->productOwner;
+        $mailVars = [
+            '{productId}' => $product->id,
+            '{title}' => $product->translation->title,
+            '{ownerId}' => $productOwner->id,
+            '{ownerEmail}' => $productOwner->email,
+            '{owner}' => !(empty($productOwner->profile->name . ' ' . $productOwner->profile->surname)) ?
+                $productOwner->profile->name . ' ' . $productOwner->profile->surname : $productOwner->profile->info,
+            '{link}' => Html::a(
+                $product->translation->title,
+                Url::toRoute('/shop/product/save?id=' . $product->id . '&languageId=' . Language::getCurrent()->id, true)),
+
+        ];
+
+        //Send mail to manager
+        $mailTemplate = $this->createMailTemplate('new-product-to-manager', $mailVars);
+        $partnerManagerEmail = \Yii::$app->getModule('shop')->partnerManagerEmail;
+
+        if (!empty($mailTemplate) && !empty($partnerManagerEmail)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $sendFrom = [\Yii::$app->get('shopMailer')->transport->getUsername() => \Yii::$app->name ?? parse_url(Url::base(true), PHP_URL_HOST)];
+
+            $this->sendMessage($sendFrom, $partnerManagerEmail, $subject, $bodyParams);
+        }
+
+        //Send mail to partner
+        $mailTemplate = $this->createMailTemplate('new-product-to-partner', $mailVars);
+        if (!empty($mailTemplate) && !empty($productOwner->email)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $sendFrom = [\Yii::$app->get('shopMailer')->transport->getUsername() => \Yii::$app->name ?? parse_url(Url::base(true), PHP_URL_HOST)];
+
+            $this->sendMessage($sendFrom, $productOwner->email, $subject, $bodyParams);
+        }
+    }
+
+    /**
+     * If a product has successfully passed moderation
+     * this email to product owner will be sent
+     * @param Product $product
+     */
+    public function sendAcceptProductToOwner(Product $product)
+    {
+
+        $productOwner = $product->productOwner;
+        $ownerEmail = $productOwner->email;
+
+        $mailVars = [
+            '{title}' => $product->translation->title,
+            '{ownerEmail}' => $ownerEmail,
+            '{owner}' => !(empty($productOwner->profile->name . ' ' . $productOwner->profile->surname)) ?
+                $productOwner->profile->name . ' ' . $productOwner->profile->surname : $productOwner->profile->info,
+            '{link}' => Html::a(
+                $product->translation->title,
+                Url::toRoute('/shop/product/save?id=' . $product->id . '&languageId=' . Language::getCurrent()->id, true)),
+
+        ];
+        $mailTemplate = $this->createMailTemplate('accept-product-to-owner', $mailVars);
+
+        if (!empty($mailTemplate)) {
+            $subject = $mailTemplate->getSubject();
+            $bodyParams = ['bodyContent' => $mailTemplate->getBody()];
+
+            $sendFrom = [\Yii::$app->get('shopMailer')->transport->getUsername() => \Yii::$app->name ?? parse_url(Url::base(true), PHP_URL_HOST)];
+
+            $this->sendMessage($sendFrom, $ownerEmail, $subject, $bodyParams);
+        }
+    }
+
+}
---
+++ UrlRule.php
@@ -0,0 +1,246 @@
+<?php
+namespace xalberteinsteinx\shop;
+
+use xalberteinsteinx\shop\common\entities\Category;
+use xalberteinsteinx\shop\common\entities\CategoryTranslation;
+use xalberteinsteinx\shop\common\entities\Product;
+use xalberteinsteinx\shop\common\entities\ProductTranslation;
+use bl\multilang\entities\Language;
+use bl\seo\entities\SeoData;
+use Yii;
+use yii\base\Object;
+use yii\web\NotFoundHttpException;
+use yii\web\Request;
+use yii\web\UrlManager;
+use yii\web\UrlRuleInterface;
+
+/**
+ * @author Gutsulyak Vadim <guts.vadim@gmail.com>
+ */
+class UrlRule extends Object implements UrlRuleInterface
+{
+    public $prefix = '';
+    private $pathInfo;
+    private $routes;
+    private $routesCount;
+    private $currentLanguage;
+    private $productRoute = 'shop/product/show';
+    private $categoryRoute = 'shop/category/show';
+
+    /**
+     * @var Language
+     */
+    private $_currentManagerLanguage;
+
+    /**
+     * Parses the given request and returns the corresponding route and parameters.
+     * @param UrlManager $manager the URL manager
+     * @param Request $request the request component
+     * @return array|bool the parsing result. The route and the parameters are returned as an array.
+     * If false, it means this rule cannot be used to parse this path info.
+     * @throws NotFoundHttpException
+     */
+    public function parseRequest($manager, $request) {
+        $this->currentLanguage = Language::getCurrent();
+        $this->pathInfo = $request->getPathInfo();
+
+        if($this->pathInfo == $this->categoryRoute || $this->pathInfo == $this->productRoute) {
+            Yii::$app->urlManager->language = $this->currentLanguage;
+            if($this->createUrl(Yii::$app->urlManager, $this->pathInfo, $request->getQueryParams())) {
+                throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
+            }
+            else if($this->pathInfo == $this->productRoute && empty($request->getQueryParams()['id'])) {
+                throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
+            }
+        }
+
+        if(!empty($this->prefix)) {
+            if(strpos($this->pathInfo, $this->prefix) === 0) {
+                $this->pathInfo = substr($this->pathInfo, strlen($this->prefix));
+            }
+            else {
+                return false;
+            }
+        }
+        $this->initRoutes($this->pathInfo);
+        if(!empty($this->prefix) && $this->routesCount == 1) {
+            return [$this->categoryRoute, []];
+        }
+        $categoryId = null;
+        for($i = 1; $i < $this->routesCount; $i++) {
+            if($i === $this->routesCount - 1) {
+                if($product = $this->findProductBySeoUrl($this->routes[$i], $categoryId)) {
+                    return [
+                        '/' . $this->productRoute,
+                        ['id' => $product->id]
+                    ];
+                }
+                else {
+                    if($category = $this->findCategoryBySeoUrl($this->routes[$i], $categoryId)) {
+                        return [
+                            '/' . $this->categoryRoute,
+                            ['id' => $category->id]
+                        ];
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+            else {
+                if($category = $this->findCategoryBySeoUrl($this->routes[$i], $categoryId)) {
+                    $categoryId = $category->id;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    private function initRoutes($pathInfo) {
+        $this->routes = explode('/', $pathInfo);
+        $this->routesCount = count($this->routes);
+    }
+    private function findProductBySeoUrl($seoUrl, $categoryId, $options = []) {
+        $productsSeoData = SeoData::find()
+            ->where([
+                'entity_name' => ProductTranslation::className(),
+                'seo_url' => $seoUrl
+            ])->all();
+        if($productsSeoData) {
+            foreach($productsSeoData as $productSeoData) {
+                if($product = Product::find()
+                    ->joinWith('translations translation')
+                    ->where(array_merge([
+                        'translation.id' => $productSeoData->entity_id,
+                        'category_id' => $categoryId,
+                        'translation.language_id' => $this->currentLanguage->id
+                    ], $options))->one()) {
+                    return $product;
+                }
+            }
+        }
+        return null;
+    }
+    private function findCategoryBySeoUrl($seoUrl, $parentId, $options = []) {
+        $categoriesSeoData = SeoData::find()
+            ->where([
+                'entity_name' => CategoryTranslation::className(),
+                'seo_url' => $seoUrl
+            ])->all();
+        if($categoriesSeoData) {
+            foreach($categoriesSeoData as $categorySeoData) {
+                if($category = Category::find()
+                    ->joinWith('translations translation')
+                    ->where(array_merge([
+                        'translation.id' => $categorySeoData->entity_id,
+                        'parent_id' => $parentId,
+                        'translation.language_id' => $this->currentLanguage->id
+                    ], $options))->one()) {
+                    return $category;
+                }
+            }
+        }
+        return null;
+    }
+    /**
+     * Creates a URL according to the given route and parameters.
+     * @param UrlManager $manager the URL manager
+     * @param string $route the route. It should not have slashes at the beginning or the end.
+     * @param array $params the parameters
+     * @return string|boolean the created URL, or false if this rule cannot be used for creating this URL.
+     */
+    public function createUrl($manager, $route, $params)
+    {
+        $pathInfo = '';
+        if($route == $this->categoryRoute && empty($params['id'])) {
+            $pathInfo = $this->prefix;
+        }
+        else if(($route == $this->productRoute || $route == $this->categoryRoute) && !empty($params['id'])) {
+            $id = $params['id'];
+            $parentId = null;
+            $this->_currentManagerLanguage = Language::findOne(['lang_id' => $manager->language]);
+
+            if($route == $this->productRoute) {
+                /** @var Product $product */
+                $product = Product::find()
+                    ->with([
+                        'translation' => function (\yii\db\ActiveQuery $query) {
+                            $query->onCondition(['language_id' => $this->_currentManagerLanguage->id]);
+                        },
+                    ])
+                    ->where(['id' => $id])
+                    ->one();
+
+                if(empty($product)) {
+                    return false;
+                }
+
+                if(!empty($product->translation->seoUrl)) {
+                    $pathInfo = $product->translation->seoUrl;
+                    $parentId = $product->category_id;
+                }
+                else {
+                    return false;
+                }
+            }
+            else if($route == $this->categoryRoute) {
+                /** @var Category $category */
+                $category = Category::find()
+                    ->with([
+                        'translation' => function (\yii\db\ActiveQuery $query) {
+                            $query->onCondition(['language_id' => $this->_currentManagerLanguage->id]);
+                        },
+                    ])
+                    ->where(['id' => $id])
+                    ->one();
+
+                if(empty($category)) {
+                    return false;
+                }
+
+                if(!empty($category->translation->seoUrl)) {
+                    $pathInfo = $category->translation->seoUrl;
+                    $parentId = $category->parent_id;
+                }
+                else {
+                    return false;
+                }
+            }
+            while($parentId != null) {
+                /** @var Category $category */
+                $category = Category::find()
+                    ->with([
+                        'translation' => function (\yii\db\ActiveQuery $query) {
+                            $query->onCondition(['language_id' => $this->_currentManagerLanguage->id]);
+                        },
+                    ])
+                    ->where(['id' => $parentId])
+                    ->one();
+
+                if(empty($category)) {
+                    return false;
+                }
+
+                if(!empty($category->translation->seoUrl)) {
+                    $pathInfo = $category->translation->seoUrl . '/' . $pathInfo;
+                    $parentId = $category->parent_id;
+                }
+                else {
+                    return false;
+                }
+            }
+            if(!empty($this->prefix)) {
+                $pathInfo = $this->prefix . '/' . $pathInfo;
+            }
+            unset($params['id']);
+        }
+        else {
+            return false;
+        }
+
+        $queryParams = (!empty(http_build_query($params))) ? '?' . http_build_query($params) : '';
+        return $pathInfo . $queryParams;
+    }
+}
