20f5f41a72d266691fb7046d6c025d04462b6515
Albert <xalbert.einsteinx@gmail.com>
2017-05-15 00:12:14 +0300

    Change VendorController views on backend.

--- backend/views/vendor/index.php
+++ backend/views/vendor/index.php
@@ -1,4 +1,5 @@
 <?php
+use rmrevin\yii\fontawesome\FA;
 use xalberteinsteinx\shop\backend\components\form\VendorImage;
 use xalberteinsteinx\shop\common\entities\Vendor;
 use yii\bootstrap\Html;
@@ -11,81 +12,57 @@ use yii\helpers\Url;
  * @var VendorImage $vendor_images
  */
 
-$this->title = Yii::t('shop', 'Product vendors');
+$this->title = Yii::t('shop', 'Vendor list');
 ?>
 
-<h1><?= Html::encode($this->title); ?></h1>
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <i class="glyphicon glyphicon-list"></i>
-                <?= Yii::t('shop', 'Vendor list'); ?>
-            </div>
-            <div>
-                <div class="panel-body">
-                    <table class="table-bordered table-condensed table-hover">
-                        <?php if (!empty($vendors)): ?>
-                            <thead>
-                            <tr>
-                                <th class="col-xs-1 col-sm-1 col-md-1"><?= 'Id' ?></th>
-                                <th class="col-xs-2 col-sm-2 col-md-2"><?= Yii::t('shop', 'Logo') ?></th>
-                                <th class="col-xs-5 col-sm-7 col-md-7"><?= Yii::t('shop', 'Title') ?></th>
-                                <th class="col-sm-2 col-md-2 text-center"><?= Yii::t('shop', 'Control') ?></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($vendors as $vendor): ?>
-                                <tr>
-                                    <td class="text-muted">
-                                        <?= $vendor->id ?>
-                                    </td>
+<div class="box">
 
-                                    <td>
-                                        <div class="text-center">
-                                            <?php if (!empty($vendor->image_name)): ?>
-                                                <?= Html::img($vendor_images->getThumb($vendor->image_name), [
-                                                        'class' => 'img-responsive',
-                                                        'data-toggle' => 'modal',
-                                                        'data-target' => '#' . $vendor->image_name
-                                                ])?>
-                                                <div id="<?= $vendor->image_name ?>" class="modal fade" role="dialog">
-                                                    <?= Html::img($vendor_images->getBig($vendor->image_name), [
-                                                            'class' => 'modal-dialog'
-                                                    ])?>
-                                                </div>
-                                            <?php else: ?>
-                                                <div class="glyphicon glyphicon-picture text-muted" data-toggle="tooltip" data-placement="top"
-                                                     title="<?= Yii::t('shop', 'No image') ?>"
-                                                     data-original-title="<?= Yii::t('shop', 'No image') ?>"></div>
-                                            <?php endif; ?>
-                                        </div>
-                                    </td>
+    <div class="box-title">
+        <h1><?= FA::i(FA::_SHIELD) . ' ' . Html::encode($this->title); ?></h1>
+        <a href="<?= Url::to(['save']); ?>" class="btn btn-xs">
+            <?= FA::i(FA::_USER_PLUS) . ' ' . Yii::t('shop', 'Add'); ?>
+        </a>
+    </div>
 
-                                    <td>
-                                        <?= $vendor->title ?>
-                                    </td>
+    <div class="box-content">
+        <table class="table">
+            <?php if (!empty($vendors)): ?>
+                <thead>
+                <tr>
+                    <th class="col-md-1"><?= 'Id' ?></th>
+                    <th class="col-md-2"><?= Yii::t('shop', 'Logo') ?></th>
+                    <th class="col-md-7"><?= Yii::t('shop', 'Title') ?></th>
+                    <th class="col-md-2 text-center"><?= Yii::t('shop', 'Control') ?></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($vendors as $vendor): ?>
+                    <tr>
+                        <td>
+                            <?= $vendor->id ?>
+                        </td>
 
-                                    <td class="text-center">
-                                        <?= \xalberteinsteinx\shop\widgets\ManageButtons::widget([
-                                            'model' => $vendor,
-                                            'deleteUrl' =>  Url::to(['remove', 'id' => $vendor->id])
-                                        ]); ?>
-                                </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                        <?php endif; ?>
-                    </table>
-                    <div class="row-fluid" style="margin-top: 15px;">
-                        <a href="<?= Url::to(['save']); ?>"
-                           class="btn btn-primary pull-right">
-                            <i class="fa fa-user-plus"></i> <?= Yii::t('shop', 'Add'); ?>
-                        </a>
-                    </div>
-                </div>
+                        <td>
+                            <?php if (!empty($vendor->image_name)): ?>
+                                <?= Html::img($vendor_images->getSmall($vendor->image_name))?>
+                            <?php else: ?>
+                                <?= FA::i(FA::_IMAGE); ?>
+                            <?php endif; ?>
+                        </td>
 
-            </div>
-        </div>
+                        <td>
+                            <?= $vendor->title ?>
+                        </td>
+
+                        <td class="text-center">
+                            <?= \xalberteinsteinx\shop\widgets\ManageButtons::widget([
+                                'model' => $vendor,
+                                'deleteUrl' =>  Url::to(['remove', 'id' => $vendor->id])
+                            ]); ?>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            <?php endif; ?>
+        </table>
     </div>
 </div>
-<?php $this->registerJs("$(\"[data-toggle='tooltip']\").tooltip();") ?>
--- backend/views/vendor/save.php
+++ backend/views/vendor/save.php
@@ -1,5 +1,7 @@
 <?php
 use marqu3s\summernote\Summernote;
+use rmrevin\yii\fontawesome\FA;
+use xalberteinsteinx\shop\backend\assets\EditProductAsset;
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
@@ -16,105 +18,108 @@ use xalberteinsteinx\shop\backend\components\form\VendorImage;
 
 $this->title = Yii::t('shop', 'Save vendor');
 
-//VendorAsset::register($this);
+EditProductAsset::register($this);
 ?>
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <i class="glyphicon glyphicon-list"></i>
-                <?= Html::encode($this->title); ?>
+<div class="box">
 
-                <?= \xalberteinsteinx\shop\widgets\LanguageSwitcher::widget([
-                    'selectedLanguage' => \bl\multilang\entities\Language::findOne($vendorTranslation->language_id)
-                ]); ?>
-            </div>
-            <div class="panel-body">
-                <?php $form = ActiveForm::begin([
-                        'method' => 'post',
-                        'options' => ['enctype' => 'multipart/form-data']
-                    ]) ?>
+    <div class="box-title">
+        <h1>
+            <?= FA::i(FA::_EDIT) . ' ' . Html::encode($this->title); ?>
+        </h1>
 
-                <div class="col-md-offset-2 col-md-8">
-                    <!--TITLE-->
-                    <?= $form->field($vendor, 'title')->textInput(['id' => 'producttranslation-title']) ?>
+        <?= \xalberteinsteinx\shop\widgets\LanguageSwitcher::widget([
+            'selectedLanguage' => \bl\multilang\entities\Language::findOne($vendorTranslation->language_id)
+        ]); ?>
+    </div>
 
-                    <!--IMAGE-->
-                    <h4><?= Yii::t('shop', 'Logo'); ?></h4>
-                    <div class="text-center">
+    <div class="box-content">
+
+        <?php $form = ActiveForm::begin([
+            'method' => 'post',
+            'options' => ['enctype' => 'multipart/form-data'],
+        ]) ?>
+        <!--TITLE-->
+        <?= $form->field($vendor, 'title')->textInput(['id' => 'producttranslation-title']) ?>
+
+        <div class="row">
+
+            <div class="col-md-6">
+                <h2><?= \Yii::t('shop', 'Basic options'); ?></h2>
+
+                <!--IMAGE-->
+                <h4><?= Yii::t('shop', 'Logo'); ?></h4>
+                <div class="row">
+                    <div class="col-md-6">
+                        <?= $form->field($vendor_image, 'imageFile')->fileInput(); ?>
+                    </div>
+                    <div class="col-md-6">
                         <?php if(!empty($vendor->image_name)): ?>
-                            <?= Html::img($vendor_image->getBig($vendor->image_name), [
+                            <?= Html::img($vendor_image->getSmall($vendor->image_name), [
                                 'class' => 'img-thumbnail thumbnail center-block'
                             ]) ?>
                         <?php else: ?>
-                            <div class="glyphicon glyphicon-picture text-muted" data-toggle="tooltip" data-placement="top"
-                                 title="<?= Yii::t('shop', 'No image') ?>"
-                                 data-original-title="<?= Yii::t('shop', 'No image') ?>"></div>
+                            <?= FA::i(FA::_IMAGE); ?>
                         <?php endif; ?>
                     </div>
-
-                    <!--IMAGE INPUT-->
-                    <div class="row">
-                        <div class="col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 text-center">
-                            <?= $form->field($vendor_image, 'imageFile')->fileInput(); ?>
-                        </div>
-                    </div>
-
-                    <!--DESCRIPTION-->
-                    <?= $form->field($vendorTranslation, 'description', [
-                        'inputOptions' => [
-                            'class' => 'form-control'
-                        ]
-                    ])->widget(Summernote::className())->label(\Yii::t('shop', 'Description'));
-                    ?>
-
-
-                    <!-- SEO -->
-                    <h2><?= \Yii::t('shop', 'SEO options'); ?></h2>
-                    <div class="seo-url">
-                        <?= $form->field($vendorTranslation, 'seoUrl', [
-                            'inputOptions' => [
-                                'class' => 'form-control',
-                                'id' => 'producttranslation-seourl'
-                            ]
-                        ])->label('SEO URL')
-                        ?>
-                        <?= Html::button(\Yii::t('shop', 'Generate'), [
-                            'id' => 'generate-seo-url',
-                            'class' => 'btn btn-primary btn-generate',
-                            'url' => Url::to('/admin/shop/product/generate-seo-url')
-                        ]); ?>
-                    </div>
-
-                    <?= $form->field($vendorTranslation, 'seoTitle', [
-                        'inputOptions' => [
-                            'class' => 'form-control'
-                        ]
-                    ])->label(\Yii::t('shop', 'SEO title'))
-                    ?>
-                    <?= $form->field($vendorTranslation, 'seoDescription', [
-                        'inputOptions' => [
-                            'class' => 'form-control'
-                        ]
-                    ])->textarea(['rows' => 3])->label(\Yii::t('shop', 'SEO description'))
-                    ?>
-                    <?= $form->field($vendorTranslation, 'seoKeywords', [
-                        'inputOptions' => [
-                            'class' => 'form-control'
-                        ]
-                    ])->textarea(['rows' => 3])->label(\Yii::t('shop', 'SEO keywords'))
-                    ?>
-
-                    <!--SUBMIT-->
-                    <?= Html::submitButton(Yii::t('shop', 'Save'), [
-                        'class' => 'btn btn-success pull-right'
-                    ]) ?>
                 </div>
 
-                <?php $form->end(); ?>
+                <!--DESCRIPTION-->
+                <?= $form->field($vendorTranslation, 'description', [
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ]
+                ])->widget(Summernote::className())->label(\Yii::t('shop', 'Description'));
+                ?>
             </div>
+            <div class="col-md-6">
+
+                <!-- SEO -->
+                <h2><?= \Yii::t('shop', 'SEO options'); ?></h2>
+                <div class="seo-url">
+
+                    <?= $form->field($vendorTranslation, 'seoUrl', [
+                        'inputOptions' => [
+                            'class' => 'form-control',
+                            'id' => 'producttranslation-seourl'
+                        ]
+                    ])->label('SEO URL')
+                    ?>
+
+                    <?= Html::button(\Yii::t('shop', 'Generate'), [
+                        'id' => 'generate-seo-url',
+                        'class' => 'btn btn-in-input',
+                        'url' => Url::to('/admin/shop/product/generate-seo-url')
+                    ]); ?>
+                </div>
+
+                <?= $form->field($vendorTranslation, 'seoTitle', [
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ]
+                ])->label(\Yii::t('shop', 'SEO title'))
+                ?>
+                <?= $form->field($vendorTranslation, 'seoDescription', [
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ]
+                ])->textarea(['rows' => 3])->label(\Yii::t('shop', 'SEO description'))
+                ?>
+                <?= $form->field($vendorTranslation, 'seoKeywords', [
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ]
+                ])->textarea(['rows' => 3])->label(\Yii::t('shop', 'SEO keywords'))
+                ?>
+
+                <!--SUBMIT-->
+                <?= Html::submitButton(Yii::t('shop', 'Save'), [
+                    'class' => 'btn btn-success pull-right'
+                ]) ?>
+            </div>
+
         </div>
+        <?php $form->end(); ?>
+
     </div>
 </div>
-<?php $this->registerJs("$(\"[data-toggle='tooltip']\").tooltip();") ?>
