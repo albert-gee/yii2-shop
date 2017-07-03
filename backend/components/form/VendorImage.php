05d35f2a33fcc9fb88039f90875e649c02516505
Albert <xalbert.einsteinx@gmail.com>
2017-07-03 19:59:47 +0300

    Add getImageName() method to VendorImage model on backend. Change Upload method().

--- backend/components/form/VendorImage.php
+++ backend/components/form/VendorImage.php
@@ -17,7 +17,6 @@ class VendorImage extends Model
 
     private $_orig_image_name;
     private $_image_name;
-    private $_image_extension = '.jpg';
     private $_category = 'shop-vendors';
 
     public function rules()
@@ -34,7 +33,8 @@ class VendorImage extends Model
         ];
     }
 
-    public function getImageName() {
+    public function getImageName()
+    {
         return $this->_image_name;
     }
 
@@ -49,37 +49,50 @@ class VendorImage extends Model
         $image = Yii::$app->shop_imagable;
 
         // save original
-        $this->_orig_image_name = $this->imageFile->baseName . $this->_image_extension;
+        $this->_orig_image_name = $this->imageFile->name;
         $this->imageFile->saveAs($image->imagesPath . $this->_orig_image_name);
         // create small, thumb & big
         $this->_image_name = $image->create($this->_category, $image->imagesPath . $this->_orig_image_name);
         // delete original
-        if(file_exists($image->imagesPath . $this->_orig_image_name))
+        if (file_exists($image->imagesPath . $this->_orig_image_name))
             unlink($image->imagesPath . $this->_orig_image_name);
     }
 
     public function Remove($image_name)
     {
-        if(!empty($image_name)) {
+        if (!empty($image_name)) {
             $dir = Yii::getAlias('@frontend/web');
-            if(file_exists($dir . $this->getBig($image_name)))
+            if (file_exists($dir . $this->getBig($image_name)))
                 unlink($dir . $this->getBig($image_name));
-            if(file_exists($dir . $this->getThumb($image_name)))
+            if (file_exists($dir . $this->getThumb($image_name)))
                 unlink($dir . $this->getThumb($image_name));
-            if(file_exists($dir . $this->getSmall($image_name)))
+            if (file_exists($dir . $this->getSmall($image_name)))
                 unlink($dir . $this->getSmall($image_name));
         }
     }
 
+    /**
+     * @param string $size
+     * @param string $name
+     * @return string
+     */
+    public function getImage(string $size, string $name)
+    {
+        $image = \Yii::$app->shop_imagable->get($this->_category, $size, $name);
+        $image = str_replace('\\', '/', $image);
+        $image = str_replace(str_replace('\\', '/', \Yii::getAlias('@frontend') . '/web'), '', $image);
+        return $image;
+
+    }
     public function getBig($image_name) {
-        return ('/images/' . $this->_category . '/' . $image_name . '-big' . $this->_image_extension);
+        return $this->getImage('big', $image_name);
     }
 
     public function getThumb($image_name) {
-        return ('/images/' . $this->_category . '/' . $image_name . '-thumb' . $this->_image_extension);
+        return $this->getImage('thumb', $image_name);
     }
 
     public function getSmall($image_name) {
-        return ('/images/' . $this->_category . '/' . $image_name . '-small' . $this->_image_extension);
+        return $this->getImage('small', $image_name);
     }
 }
