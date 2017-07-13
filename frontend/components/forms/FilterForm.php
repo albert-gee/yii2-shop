51a9765a6105290d91f3890aff9cbd1eaa12a676
Albert <xalbert.einsteinx@gmail.com>
2017-07-13 06:36:31 +0300

    Add FilterForm class.

---
+++ frontend/components/forms/FilterForm.php
@@ -0,0 +1,29 @@
+<?php
+namespace xalberteinsteinx\shop\frontend\components\forms;
+
+use yii\base\Model;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class FilterForm extends Model
+{
+    /**
+     * @var integer
+     */
+    public $vendor_id;
+    /**
+     * @var integer
+     */
+    public $availability_id;
+
+    /**
+     * Returns the validation rules for attributes.
+     */
+    public function rules()
+    {
+        return [
+            [['vendor_id', 'availability_id'], 'integer'],
+        ];
+    }
+}
